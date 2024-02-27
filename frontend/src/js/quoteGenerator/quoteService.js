328068d0fcae43397b1a48d8f1c35832be86750c
Metameg <metameg8@gmail.com>
2024-02-26 20:12:15 -0700

    fixed csrf token not included in quote generation request

--- frontend/src/js/quoteGenerator/quoteService.js
+++ frontend/src/js/quoteGenerator/quoteService.js
@@ -2,13 +2,18 @@ const quoteService = {
     submitQuoteData: function(url, quoteData) {
         var initialDeferred = $.Deferred();
         var progressDeferred = $.Deferred();
+        var csrfToken = $('input[name="csrf_token"]').val();
 
         return new Promise((resolve, reject) => {
-             $.ajax({
+            console.log(quoteData, url);
+            $.ajax({
                 url: url,  
                 type: 'POST',
                 contentType: 'application/json',
                 data: quoteData,
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken
+                },
                 success: function(data) {
                     console.log("data: " + data);
                     resolve(data);
@@ -16,30 +21,31 @@ const quoteService = {
                     // return data;
                 },
                 error: function(error) {
+                    console.log("Error!");
                     // initialDeferred.reject();
                     reject(error);
                 },
                 resetForm: true
             });
             // Periodically check and display progress
-            var checkProgress = function() {
-                return new Promise(function(resolve) {
-                    var checkProgressInterval = setInterval(function() {
-                        $.ajax({
-                            type: 'GET',
-                            url: '/get_progress', 
-                            success: function(progress) {
-                                console.log(parseInt(progress));
-                                if (parseInt(progress) >= 20) {
-                                    clearInterval(checkProgressInterval);
-                                    progressDeferred.resolve();
-                                    resolve();
-                                }
-                            }
-                        });
-                    }, 1000);  // Check every 1 second
-                });
-            };
+            // var checkProgress = function() {
+            //     return new Promise(function(resolve) {
+            //         var checkProgressInterval = setInterval(function() {
+            //             $.ajax({
+            //                 type: 'GET',
+            //                 url: '/get_progress', 
+            //                 success: function(progress) {
+            //                     console.log(parseInt(progress));
+            //                     if (parseInt(progress) >= 20) {
+            //                         clearInterval(checkProgressInterval);
+            //                         progressDeferred.resolve();
+            //                         resolve();
+            //                     }
+            //                 }
+            //             });
+            //         }, 1000);  // Check every 1 second
+            //     });
+            // };
         }); 
 
     }
