2e7f0fbf464d351a035bde677233713678bab592
Metameg <ametzger08@gmail.com>
2024-06-18 23:13:01 -0600

    video file selected validator implemented

--- frontend/src/js/renders/renderFormController.js
+++ frontend/src/js/renders/renderFormController.js
@@ -1,4 +1,5 @@
 import { renderService } from './renderService.js';
+import { contains_video  } from '../shared/utils.js';
 import renderFormDOM from './renderFormUI.js';
 import videoStylerUI from '../videoStyler/videoStylerUI.js';
 import uploadMediaUI from "../mediaUploader/uploadMediaUI.js";
@@ -6,7 +7,7 @@ import uploadMediaUI from "../mediaUploader/uploadMediaUI.js";
 
 function appendMediaData(renderData) {
     var selectedMedia = uploadMediaUI.getSelectedMedia();
-    
+
     selectedMedia.forEach(function(media) {
         // Append each media item as a key-value pair in the FormData object
         renderData.append('selectedMedia[]', media);
@@ -19,39 +20,50 @@ export function configureRenderForm() {
     renderFormDOM.configureDOM();
     videoStylerUI.configureDOM();
     const renderForm = document.getElementById('render-form');
+    const noVideoMsg = document.getElementById('no-video-msg');
 
     renderForm.addEventListener('submit', async function(event) {
         
         event.preventDefault();
         
-        
         var renderData = new FormData($('#render-form')[0]);
         renderData = appendMediaData(renderData);
-       
-        var renderDataObject = {};
-        renderData.forEach(function(value, key){
-            if (!renderDataObject[key]) {
-                renderDataObject[key] = value;
-            } else {
-                if (!Array.isArray(renderDataObject[key])) {
-                    renderDataObject[key] = [renderDataObject[key]];
+        
+        
+        const hasVideo = contains_video(renderData.getAll('selectedMedia[]'));
+        
+
+        if (hasVideo) {
+            noVideoMsg.style.display = 'none';
+            var renderDataObject = {};
+            renderData.forEach(function(value, key){
+                if (!renderDataObject[key]) {
+                    renderDataObject[key] = value;
+                } else {
+                    if (!Array.isArray(renderDataObject[key])) {
+                        renderDataObject[key] = [renderDataObject[key]];
+                    }
+                    renderDataObject[key].push(value);
                 }
-                renderDataObject[key].push(value);
+            });
+
+
+            delete renderDataObject.mediaPath;
+            delete renderDataObject.csrf_token;
+            $('#video-content').html('');
+            renderFormDOM.showVideoLoading();
+
+            try {
+                const response = await renderService.postFormData(renderDataObject);
+                $('#video-content').html(response);
+            } catch (error) {
+                // Handle errors if needed
+                console.error(error);
             }
-        });
+        }
 
-
-        delete renderDataObject.mediaPath;
-        delete renderDataObject.csrf_token;
-        $('#video-content').html('');
-        renderFormDOM.showVideoLoading();
-
-        try {
-            const response = await renderService.postFormData(renderDataObject);
-            $('#video-content').html(response);
-        } catch (error) {
-            // Handle errors if needed
-            console.error(error);
+        else {
+            noVideoMsg.style.display = 'block';
         }
     });
 }
--- frontend/src/js/shared/utils.js
+++ frontend/src/js/shared/utils.js
@@ -1,3 +1,5 @@
+
+
 function enableFromCheckbox (checkbox, dropdown) {
     checkbox.addEventListener('change', function () {
         dropdown.disabled = !checkbox.checked;
@@ -16,4 +18,20 @@ function updateHiddenBln (hiddenElement, checkboxElement) {
     });
 }
 
-export {enableFromCheckbox, notEnableFromCheckbox, updateHiddenBln}
+function contains_video(paths) {
+    const mime = require('mime');
+    // const path = '/%252Fhome%252Fwicker%252FOmniClip-Dev%252FOmniClip%252Fapp%252Ftools%252F..%252Ftemp%252Fguest/glow_480p(1).mp4';
+
+    // Decode the URL-encoded path
+    const decodedPath = decodeURIComponent(paths);
+    const mimeType = mime.getType(decodedPath);
+
+    if (mimeType && mimeType.startsWith('video/')) {
+        return true; 
+    }
+    console.log('MIME type:', mimeType);
+
+    return false;
+}
+
+export {enableFromCheckbox, notEnableFromCheckbox, updateHiddenBln, contains_video}
